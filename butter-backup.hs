78f4a3d4b1c9690e4a38f51c90c2b6cdcedfb007
Max Görner <max@familie-goerner.eu>
2021-03-08 09:00:56 +0100

    Rufe Shell-Skript in Hello-World auf

--- butter-backup.hs
+++ butter-backup.hs
@@ -1,2 +1,13 @@
+import System.Process (readProcess)
+
 main :: IO ()
-main = putStrLn "Hello, World!"
+main = do
+    results <- splitLines runShell
+    -- print each line in reverse
+    mapM_ (putStrLn . reverse) results
+
+runShell :: IO String
+runShell = readProcess "./hello-world.sh" [] []
+
+splitLines :: IO String -> IO [String]
+splitLines stdout = fmap lines stdout  -- fmap a b == a <$> b
