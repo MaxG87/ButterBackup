0285fff4f2ff7a731254ae28bec9feffd229bebc
Max Görner <max@familie-goerner.eu>
2021-03-11 20:40:40 +0100

    Kopiere CLI-Parser-Beispiel aus Dokumentation

--- app/butter-backup.hs
+++ app/butter-backup.hs
@@ -1,7 +1,25 @@
+{-# LANGUAGE DataKinds          #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE FlexibleInstances  #-}  -- One more extension.
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE StandaloneDeriving #-}  -- To derive Show
+{-# LANGUAGE TypeOperators      #-}
+
+import Options.Generic
 import System.Process (readProcess)
 
-main :: IO ()
+data Example w = Example
+    { foo :: w ::: Int    <?> "Documentation for the foo flag"
+    , bar :: w ::: Double <?> "Documentation for the bar flag"
+    } deriving (Generic)
+
+instance ParseRecord (Example Wrapped)
+deriving instance Show (Example Unwrapped)
+
 main = do
+    x <- unwrapRecord ""  -- string argument is error message
+    print (x :: Example Unwrapped)
+
     results <- splitLines runShell
     -- print each line in reverse
     mapM_ (putStrLn . reverse) results
