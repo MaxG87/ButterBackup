028db7bc5eeb0a2d21d2c94ec2fd6ce34744bc95
Max Görner <max@familie-goerner.eu>
2021-03-17 22:01:50 +0100

    Parse CLI-Argumente explizit, ohne optparse

--- app/butter-backup.hs
+++ app/butter-backup.hs
@@ -1,31 +1,53 @@
-{-# LANGUAGE DataKinds          #-}
-{-# LANGUAGE DeriveGeneric      #-}
-{-# LANGUAGE FlexibleInstances  #-}  -- One more extension.
-{-# LANGUAGE OverloadedStrings  #-}
-{-# LANGUAGE StandaloneDeriving #-}  -- To derive Show
-{-# LANGUAGE TypeOperators      #-}
-
-import Options.Generic
+import Control.Monad
+import System.Environment
 import System.Process (readProcess)
 
-data Example w = Example
-    { foo :: w ::: Int    <?> "Documentation for the foo flag"
-    , bar :: w ::: Double <?> "Documentation for the bar flag"
-    } deriving (Generic)
+data CLIArguments = PrintHelp | FilePath
+    deriving (Show)
+newtype CLIArgParseError = CLIArgParseError String
+    deriving (Show)
+type CLIArgParseResult = Either CLIArguments CLIArgParseError
 
-instance ParseRecord (Example Wrapped)
-deriving instance Show (Example Unwrapped)
+main :: IO ()
+main = join $ fmap printArgs args
+    where args = fmap parseArgs getArgs
 
-main = do
-    x <- unwrapRecord ""  -- string argument is error message
-    print (x :: Example Unwrapped)
+printArgs :: CLIArgParseResult -> IO ()
+printArgs args = print $ case args of
+        Left PrintHelp -> "Help"
+        Left FilePath -> "foobar"
+        Right (CLIArgParseError str) -> str
 
-    results <- splitLines runShell
-    -- print each line in reverse
-    mapM_ (putStrLn . reverse) results
+    -- x <- getArgs
+    -- print $ case x of
+    --     (x:xs) -> x
+    --     [] -> "Empty Args"
 
-runShell :: IO String
-runShell = readProcess "./hello-world.sh" [] []
+    -- my_variable <- parseArgs x
+    -- print $ case x of
+    --     (x:xs) -> "blablubb"
+    --     [] -> "Again empty args"
 
-splitLines :: IO String -> IO [String]
-splitLines stdout = fmap lines stdout  -- fmap a b == a <$> b
+    -- results <- splitLines runShell
+    -- -- print each line in reverse
+    -- mapM_ (putStrLn . reverse) results
+
+
+parseArgs :: [String] -> CLIArgParseResult
+parseArgs x = case x of
+    ["-h"] -> Left PrintHelp
+    ["--help"] -> Left PrintHelp
+    ("--config":_:[]) -> Left FilePath 
+    _ -> Right $ CLIArgParseError "CLIArgParseError"
+-- parseArgs ["-h"]  = Left (printUsage >> exitWith ExitSuccess)
+-- parseArgs ["--help"]  = Left (printUsage >> exitWith ExitSuccess)
+-- parseArgs ["--config", path] = Right path
+-- parseArgs _  = error "ABC"
+
+-- printUsage = putStrLn "Usage"
+
+-- runShell :: IO String
+-- runShell = readProcess "./hello-world.sh" [] []
+
+-- splitLines :: IO String -> IO [String]
+-- splitLines stdout = fmap lines stdout  -- fmap a b == a <$> b
