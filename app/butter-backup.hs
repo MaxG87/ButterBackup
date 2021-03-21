b97cca230c3346978dfe34a88295ddda83604842
Max Görner <max@familie-goerner.eu>
2021-03-21 20:38:24 +0100

    Lese Config in parseArgs ein
    Es war nicht möglich, das Single Responsibility Principle im
    CLI parsen umzusetzen. Durch engeres befolgen von
    https://wiki.haskell.org/Tutorials/Programming_Haskell/Argument_handling
    und damit durch Verletzung des SRP gelingt es nun, die CLI-Argumente zu
    parsen und eine Config-Datei einzulesen.

--- app/butter-backup.hs
+++ app/butter-backup.hs
@@ -1,53 +1,42 @@
+{-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -Werror #-}
+{-# OPTIONS_GHC -O2 #-}
+
 import Control.Monad
 import System.Environment
+import System.Exit
 import System.Process (readProcess)
-
-data CLIArguments = PrintHelp | FilePath
-    deriving (Show)
-newtype CLIArgParseError = CLIArgParseError String
-    deriving (Show)
-type CLIArgParseResult = Either CLIArguments CLIArgParseError
+import qualified Data.ByteString.Lazy as B
 
 main :: IO ()
-main = join $ fmap printArgs args
-    where args = fmap parseArgs getArgs
+main = do
+    config <- fmap parseArgs getArgs
+    join $ fmap print config
 
-printArgs :: CLIArgParseResult -> IO ()
-printArgs args = print $ case args of
-        Left PrintHelp -> "Help"
-        Left FilePath -> "foobar"
-        Right (CLIArgParseError str) -> str
+    results <- fmap lines runShell
+    mapM_ (putStrLn . reverse) results
 
-    -- x <- getArgs
-    -- print $ case x of
-    --     (x:xs) -> x
-    --     [] -> "Empty Args"
+parseArgs :: [String] -> IO B.ByteString
+parseArgs ["-h"]               = printUsage >> exit
+parseArgs ["--help"]           = printUsage >> exit
+parseArgs ["-v"]               = printVersion >> exit
+parseArgs ["--version"]        = printVersion >> exit
+parseArgs []                   = readConfig "~/.config/butter-backup.cfg"
+parseArgs ("--config":path:[]) = readConfig path
+parseArgs _                    = printUsage >> Main.die
 
-    -- my_variable <- parseArgs x
-    -- print $ case x of
-    --     (x:xs) -> "blablubb"
-    --     [] -> "Again empty args"
-
-    -- results <- splitLines runShell
-    -- -- print each line in reverse
-    -- mapM_ (putStrLn . reverse) results
+readConfig :: String -> IO B.ByteString
+readConfig path = B.readFile path
 
 
-parseArgs :: [String] -> CLIArgParseResult
-parseArgs x = case x of
-    ["-h"] -> Left PrintHelp
-    ["--help"] -> Left PrintHelp
-    ("--config":_:[]) -> Left FilePath 
-    _ -> Right $ CLIArgParseError "CLIArgParseError"
--- parseArgs ["-h"]  = Left (printUsage >> exitWith ExitSuccess)
--- parseArgs ["--help"]  = Left (printUsage >> exitWith ExitSuccess)
--- parseArgs ["--config", path] = Right path
--- parseArgs _  = error "ABC"
+printUsage :: IO ()
+printUsage = putStrLn "Usage"
+printVersion :: IO ()
+printVersion = putStrLn "Usage"
+exit :: IO a
+exit = exitWith ExitSuccess
+die :: IO a
+die = exitWith $ ExitFailure 1
 
--- printUsage = putStrLn "Usage"
-
--- runShell :: IO String
--- runShell = readProcess "./hello-world.sh" [] []
-
--- splitLines :: IO String -> IO [String]
--- splitLines stdout = fmap lines stdout  -- fmap a b == a <$> b
+runShell :: IO String
+runShell = readProcess "./hello-world.sh" [] []
