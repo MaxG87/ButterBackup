45622818bc86db6033dc487e6f406ec807505ed0
Max Görner <max@familie-goerner.eu>
2021-04-13 21:01:37 +0200

    Teste das Parsen der Kommandozeilenargumente

---
+++ tests/test_cli.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from unittest import mock
+
+from hypothesis import given
+from hypothesis import strategies as st
+
+from butter_backup import __main__ as bb
+
+
+def test_no_args_parsing() -> None:
+    default_config = bb.DEFAULT_CONFIG
+    with mock.patch("sys.argv", ["butter-backup"]):
+        config = bb.parse_args()
+    assert config == default_config
+
+
+@given(config=st.text(min_size=1))
+def test_parse_args_returns_passed_file(config: str) -> None:
+    with mock.patch("sys.argv", ["butter-backup", "--config", config]):
+        parsed_config = bb.parse_args()
+    assert Path(config) == parsed_config
