a83789846233a60491512999363a5b4222af606e
Max Görner <max@familie-goerner.eu>
2021-04-16 17:09:56 +0200

    Extrahiere Strategie für Konfigurationen
    Diese Strategie wird auch in einigen anderen Tests benötigt werden.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,6 +6,8 @@ from hypothesis import strategies as st
 
 from butter_backup import __main__ as bb
 
+path_to_config_files = st.text(min_size=1)
+
 
 def test_no_args_parsing() -> None:
     default_config = bb.DEFAULT_CONFIG
@@ -14,7 +16,7 @@ def test_no_args_parsing() -> None:
     assert config == default_config
 
 
-@given(config=st.text(min_size=1))
+@given(config=path_to_config_files)
 def test_parse_args_returns_passed_file(config: str) -> None:
     with mock.patch("sys.argv", ["butter-backup", "--config", config]):
         parsed_config = bb.parse_args()
