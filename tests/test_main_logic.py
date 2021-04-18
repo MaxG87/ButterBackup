54d154db3c7d6bc24b8d064791f7c394411bfabd
Max Görner <max@familie-goerner.eu>
2021-04-18 09:53:42 +0200

    Füge vergessene Typannotation hinzu

--- tests/test_main_logic.py
+++ tests/test_main_logic.py
@@ -5,7 +5,7 @@ import pytest
 from butter_backup import __main__ as bb
 
 
-def test_useful_error_on_missing_file_name():
+def test_useful_error_on_missing_file_name() -> None:
     missing_cfg = Path("/path/to/nowhere/butter-backup.cfg")
     with pytest.raises(SystemExit) as sysexit:
         bb.load_configuration(missing_cfg)
