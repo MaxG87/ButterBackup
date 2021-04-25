56236ca33528fff707ad140a0ed6a9374a8f9e4c
Max Görner <max@familie-goerner.eu>
2021-04-25 19:42:47 +0200

    Verschiebe Tests für MountedDevice in eigene Datei
    Es ist eine bewusste Entscheidung, `butter-backup` als einzelnes großes
    Skript zu belassen. Die Tests selbst können allerdings auf verschiedene
    Dateien aufgeteilt werden, um etwas Übersichtlichkeit zu schaffen. Dies
    geschieht mit diesem Commit.

--- tests/test_main_logic.py
+++ tests/test_main_logic.py
@@ -15,17 +15,6 @@ def mounted_directories():
             bb.run_cmd(cmd=f"sudo umount {mountpoint}")
 
 
-@pytest.fixture
-def btrfs_device():
-    btrfs_dev_size = 128 * 1024 ** 2  # ~109MiB is the minimum size for BtrFS
-    with TemporaryDirectory() as tempdir:
-        btrfs_device = Path(tempdir) / "btrfs-test-device.iso"
-        with btrfs_device.open("wb") as fh:
-            fh.write(bytes(btrfs_dev_size))
-        bb.run_cmd(cmd=f"sudo mkfs.btrfs {btrfs_device}")
-        yield btrfs_device
-
-
 def test_useful_error_on_missing_file_name() -> None:
     missing_cfg = Path("/path/to/nowhere/butter-backup.cfg")
     with pytest.raises(SystemExit) as sysexit:
@@ -54,33 +43,3 @@ def test_unmount_device(btrfs_device) -> None:
         bb.mount_btrfs_device(btrfs_device, Path(mountpoint))
         bb.unmount_device(btrfs_device)
         assert not bb.is_mounted(btrfs_device)
-
-
-def test_mounted_device(btrfs_device) -> None:
-    with bb.MountedDevice(btrfs_device) as md:
-        assert md.exists()
-        assert md.is_dir()
-        assert bb.is_mounted(btrfs_device)
-        assert md in bb.get_mounted_devices().values()
-    assert not md.exists()
-    assert not bb.is_mounted(btrfs_device)
-    assert md not in bb.get_mounted_devices().values()
-
-
-def test_mounted_device_takes_over_already_mounted_device(btrfs_device) -> None:
-    with TemporaryDirectory() as td:
-        bb.mount_btrfs_device(btrfs_device, td)
-        with bb.MountedDevice(btrfs_device) as md:
-            assert bb.is_mounted(btrfs_device)
-            assert md == bb.get_mounted_devices()[str(btrfs_device)]
-        assert not bb.is_mounted(btrfs_device)
-
-
-def test_mounted_device_fails_on_not_unmountable_device() -> None:
-    for device, mount_point in bb.get_mounted_devices().items():
-        if mount_point == Path("/"):
-            root = device
-            break
-    with pytest.raises(UnboundLocalError):
-        with bb.MountedDevice(root):
-            pass
