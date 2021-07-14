49a4e26ae83f24ceac5011886055d15288d54050
Max Görner <max@familie-goerner.eu>
2021-07-14 18:43:37 +0200

    Füge Funktion hinzu um einzelne Befehle zu pipen
    Das wird benötigt, um bei DecryptedDevice die explizite Pipe loszuwerden.

--- src/butter_backup/shell_interface.py
+++ src/butter_backup/shell_interface.py
@@ -5,7 +5,14 @@ from pathlib import Path
 from typing import List, Optional, Union
 
 _CMD_LIST = Union[List[str], List[Path], List[Union[str, Path]]]
+_LISTS_OF_CMD_LIST = Union[
+    List[List[str]], List[List[Path]], List[List[Union[str, Path]]]
+]
 StrPathList = List[Union[str, Path]]
+
+
+class ShellInterfaceError(ValueError):
+    pass
 
 
 def run_cmd(
@@ -18,3 +25,28 @@ def run_cmd(
         env = {}
     result = subprocess.run(cmd, capture_output=capture_output, check=True, env=env)
     return result
+
+
+def run_piped_commands(
+    *,
+    cmds: _LISTS_OF_CMD_LIST,
+    env: Optional[dict[str, str]] = None,
+    capture_output: bool = False
+) -> subprocess.CompletedProcess:
+    if env is None:
+        env = {}
+    if len(cmds) < 2:
+        raise ShellInterfaceError("Mindestens zwei Shell-Kommandos erwartet!")
+
+    cmd = cmds.pop(0)
+    new_proc = subprocess.run(cmd, stdout=subprocess.PIPE, check=True, env=env)
+    for cur_cmd in cmds:
+        old_proc = new_proc
+        new_proc = subprocess.run(
+            cur_cmd,
+            input=old_proc.stdout,
+            stdout=subprocess.PIPE,
+            check=True,
+            env=env,
+        )
+    return new_proc
