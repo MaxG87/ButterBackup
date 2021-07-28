db55670ca1f0ea47167773ad00898de13df5eb8a
Max Görner <max@familie-goerner.eu>
2021-07-28 15:16:35 +0200

    Entferne tests für parse_args

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,43 +3,15 @@ from tempfile import NamedTemporaryFile, TemporaryDirectory
 from unittest import mock
 
 import pytest
-from hypothesis import given
-from hypothesis import strategies as st
 from typer.testing import CliRunner
 
-from butter_backup import __main__ as bb
 from butter_backup import cli
 from butter_backup.cli import app
-
-path_to_config_files = st.text(st.characters(whitelist_categories="LN"), min_size=1)
 
 
 @pytest.fixture
 def runner():
     return CliRunner(mix_stderr=False)
-
-
-def test_no_args_parsing() -> None:
-    default_config = bb.DEFAULT_CONFIG
-    with mock.patch("sys.argv", ["butter-backup"]):
-        config = bb.parse_args()
-    assert config == default_config
-
-
-@given(cfg=path_to_config_files, xdg_config=path_to_config_files)
-def test_parse_args_returns_passed_file(cfg: str, xdg_config: str) -> None:
-    with mock.patch("sys.argv", ["butter-backup", "--config", cfg]):
-        with mock.patch("os.getenv", {"XDG_CONFIG_HOME": xdg_config}.get):
-            parsed_config = bb.parse_args()
-    assert Path(cfg) == parsed_config
-
-
-@given(xdg_config=path_to_config_files)
-def test_parse_args_returns_xdg_config_home(xdg_config: str) -> None:
-    with mock.patch("sys.argv", ["butter-backup"]):
-        with mock.patch("os.getenv", {"XDG_CONFIG_HOME": xdg_config}.get):
-            parsed_config = bb.parse_args()
-    assert Path(xdg_config) / bb.DEFAULT_CONFIG_NAME == parsed_config
 
 
 def test_get_default_config_path_refuses_missing_xdg_config() -> None:
