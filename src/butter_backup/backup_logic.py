7145113feda86bba7fbfbc6ad32a0f693b8f46bc
Max Görner <max@familie-goerner.eu>
2021-11-11 15:19:21 +0100

    Stoppe, ungelistete Zieldateien zu löschen

--- src/butter_backup/backup_logic.py
+++ src/butter_backup/backup_logic.py
@@ -33,12 +33,6 @@ def do_butter_backup(cfg: cp.ButterConfig) -> None:
                 rsync_folder(src, dest)
 
             files_dest = backup_root / cfg.files_dest
-            all_files = set(files_dest.glob("*") if files_dest.exists() else [])
-            files_of_concern = {
-                backup_root / cfg.files_dest / file.name for file in cfg.files
-            }
-            for file in all_files - files_of_concern:
-                file.unlink()
             for src in cfg.files:
                 rsync_file(src, files_dest)
 
