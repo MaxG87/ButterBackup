898ac11ac678844fc4832cfdede69baaa5ae8a49
Max Görner <max@familie-goerner.eu>
2022-04-26 20:53:32 +0200

    Schreibe Strategie zu composite-Strategie um
    Dadurch werden Inkonsistenzen im Aufruf verhindert. So müssen alle
    Strategien als Funktionsaufruf verwendet werden.

--- tests/test_config_parser.py
+++ tests/test_config_parser.py
@@ -14,21 +14,28 @@ from butter_backup import config_parser as cp
 SUCCESS_CODES = {0, None}
 NOF_FOLDER_BACKUP_MAPPING_ELEMS = 2
 
-valid_unparsed_configs = st.builds(
-    dict,
-    UUID=st.uuids().map(str),
-    PassCmd=st.text(),
-    Folders=st.lists(
-        st.lists(
-            st.text(min_size=1),
-            min_size=NOF_FOLDER_BACKUP_MAPPING_ELEMS,
-            max_size=NOF_FOLDER_BACKUP_MAPPING_ELEMS,
-        ),
-    ),
-    Files=st.fixed_dictionaries(
-        {"destination": st.text(), "files": st.lists(st.text(min_size=1))}
-    ),
-)
+
+@st.composite
+def valid_unparsed_configs(draw):
+    config = draw(
+        st.fixed_dictionaries(
+            {
+                "UUID": st.uuids().map(str),
+                "PassCmd": st.text(),
+                "Folders": st.lists(
+                    st.lists(
+                        st.text(min_size=1),
+                        min_size=NOF_FOLDER_BACKUP_MAPPING_ELEMS,
+                        max_size=NOF_FOLDER_BACKUP_MAPPING_ELEMS,
+                    ),
+                ),
+                "Files": st.fixed_dictionaries(
+                    {"destination": st.text(), "files": st.lists(st.text(min_size=1))}
+                ),
+            }
+        )
+    )
+    return config
 
 
 def test_useful_error_on_missing_file_name() -> None:
@@ -106,13 +113,15 @@ def test_load_configuration_warns_on_non_dict_item() -> None:
 
 
 @given(
-    incomplete_cfg=valid_unparsed_configs.flatmap(
+    incomplete_cfg=valid_unparsed_configs()
+    .flatmap(
         lambda cfg_dict: st.lists(
             st.sampled_from(sorted(cfg_dict.items())),
             unique_by=lambda tup: tup[0],  # type: ignore
             max_size=len(cfg_dict) - 1,
         )
-    ).map(dict)
+    )
+    .map(dict)
 )
 def test_parsing_config_fails_on_missing_keys(incomplete_cfg) -> None:
     with pytest.raises(SystemExit) as sysexit:
@@ -121,7 +130,7 @@ def test_parsing_config_fails_on_missing_keys(incomplete_cfg) -> None:
 
 
 @given(
-    config=valid_unparsed_configs,
+    config=valid_unparsed_configs(),
     invalid_folder_mapping=st.lists(st.text()).filter(
         lambda lst: len(lst) != NOF_FOLDER_BACKUP_MAPPING_ELEMS
     ),
@@ -135,7 +144,7 @@ def test_parsing_config_fails_on_malformed_folder_backup_mappings(
     assert sysexit.value.code not in SUCCESS_CODES
 
 
-@given(config=valid_unparsed_configs)
+@given(config=valid_unparsed_configs())
 def test_parsing_config_parses(config) -> None:
     cfg = cp.ParsedButterConfig.from_dict(config)
     assert str(cfg.uuid) == config["UUID"]
@@ -145,7 +154,7 @@ def test_parsing_config_parses(config) -> None:
     assert cfg.files == set(config["Files"]["files"])
 
 
-@given(base_config=valid_unparsed_configs)
+@given(base_config=valid_unparsed_configs())
 def test_butter_config_accepts_raw_config(base_config):
     with TemporaryDirectory() as src_folder:
         with TemporaryDirectory() as dest:
@@ -175,7 +184,7 @@ def test_butter_config_uuid_is_mapname(date, uuid) -> None:
     assert str(cfg.uuid) == cfg.map_name()
 
 
-@given(base_config=valid_unparsed_configs)
+@given(base_config=valid_unparsed_configs())
 def test_butter_config_rejects_missing_folder_src(base_config):
     folder_dest = "folder-destination"
     with TemporaryDirectory() as src:
@@ -195,7 +204,7 @@ def test_butter_config_rejects_missing_folder_src(base_config):
     assert src in sysexit.value.code  # type: ignore
 
 
-@given(base_config=valid_unparsed_configs)
+@given(base_config=valid_unparsed_configs())
 def test_butter_config_rejects_non_dir_src(base_config):
     folder_dest = "folder-destination"
     with NamedTemporaryFile() as src:
@@ -214,7 +223,7 @@ def test_butter_config_rejects_non_dir_src(base_config):
     assert src.name in sysexit.value.code  # type: ignore
 
 
-@given(base_config=valid_unparsed_configs)
+@given(base_config=valid_unparsed_configs())
 def test_butter_config_rejects_missing_file_src(base_config):
     with NamedTemporaryFile() as src:
         pass
@@ -228,7 +237,7 @@ def test_butter_config_rejects_missing_file_src(base_config):
     assert src.name in sysexit.value.code  # type: ignore
 
 
-@given(base_config=valid_unparsed_configs)
+@given(base_config=valid_unparsed_configs())
 def test_butter_config_rejects_non_file_src(base_config):
     with TemporaryDirectory() as src:
         base_config["Files"]["files"] = [src]
@@ -241,7 +250,7 @@ def test_butter_config_rejects_non_file_src(base_config):
     assert src in sysexit.value.code  # type: ignore
 
 
-@given(base_config=valid_unparsed_configs)
+@given(base_config=valid_unparsed_configs())
 def test_butter_config_expands_user(base_config):
     with TemporaryDirectory() as dest:
         pass
@@ -260,7 +269,7 @@ def test_butter_config_expands_user(base_config):
     assert Path(src_file.name).expanduser() in cfg.files
 
 
-@given(base_config=valid_unparsed_configs)
+@given(base_config=valid_unparsed_configs())
 def test_butter_config_rejects_duplicate_src(base_config):
     folder_dest = "folder-destination"
     with TemporaryDirectory() as src:
@@ -278,7 +287,7 @@ def test_butter_config_rejects_duplicate_src(base_config):
         assert src in sysexit.value.code  # type: ignore
 
 
-@given(base_config=valid_unparsed_configs)
+@given(base_config=valid_unparsed_configs())
 def test_butter_config_rejects_duplicate_dest(base_config):
     folder_dest = "folder-destination"
     with TemporaryDirectory() as src1:
@@ -297,7 +306,7 @@ def test_butter_config_rejects_duplicate_dest(base_config):
             assert folder_dest in sysexit.value.code  # type: ignore
 
 
-@given(base_config=valid_unparsed_configs)
+@given(base_config=valid_unparsed_configs())
 def test_butter_config_rejects_file_dest_collision(base_config):
     dest_dir = "destination-directory"
     base_config["Folders"] = [
@@ -314,7 +323,7 @@ def test_butter_config_rejects_file_dest_collision(base_config):
         assert dest_dir in sysexit.value.code  # type: ignore
 
 
-@given(base_config=valid_unparsed_configs)
+@given(base_config=valid_unparsed_configs())
 def test_butter_config_rejects_filename_collision(base_config):
     file_name = "my-file-name"
     base_config["Folders"] = []
@@ -331,7 +340,7 @@ def test_butter_config_rejects_filename_collision(base_config):
             assert file_name in sysexit.value.code  # type: ignore
 
 
-@given(base_config=valid_unparsed_configs)
+@given(base_config=valid_unparsed_configs())
 def test_butter_config_as_dict_roundtrip(base_config):
     parsed_1 = cp.ParsedButterConfig.from_dict(base_config)
     parsed_2 = cp.ParsedButterConfig.from_dict(parsed_1.as_dict())
