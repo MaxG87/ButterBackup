5f3eca5da0ac84462525820afcab80ce2b1b2740
Max Görner <max@familie-goerner.eu>
2022-06-12 22:41:27 +0200

    Erweitere Backuplogiktests um Backup mit Veränderung

--- tests/test_backup_logic.py
+++ tests/test_backup_logic.py
@@ -1,6 +1,9 @@
 import datetime as dt
+import time
 from pathlib import Path
 from typing import Iterable
+
+import pytest
 
 from butter_backup import backup_logic as bl
 from butter_backup import device_managers as dm
@@ -16,14 +19,22 @@ def list_files_recursively(path: Path) -> Iterable[Path]:
             yield file_or_folder
 
 
-def test_do_backup_for_butterbackend(encrypted_btrfs_device) -> None:
+@pytest.mark.parametrize(
+    "source_directories",
+    [[FIRST_BACKUP], [SECOND_BACKUP], [FIRST_BACKUP, SECOND_BACKUP]],
+)
+def test_do_backup_for_butterbackend(
+    source_directories, encrypted_btrfs_device
+) -> None:
     # Due to testing a timestamp, this test has a small chance to fail around
     # midnight. Since this is a hobby project, paying attention if the test
     # executes around midnight is a viable solution.
     empty_config, device = encrypted_btrfs_device
     folder_dest_dir = "some-folder-name"
-    config = empty_config.copy(update={"Folders": {FIRST_BACKUP: folder_dest_dir}})
-    bl.do_backup(config)
+    for source_dir in source_directories:
+        config = empty_config.copy(update={"Folders": {source_dir: folder_dest_dir}})
+        bl.do_backup(config)
+        time.sleep(1)  # prevent conflicts in snapshot names
     with dm.decrypted_device(device, config.DevicePassCmd) as decrypted:
         with dm.mounted_device(decrypted) as mount_dir:
             latest_folder = sorted(mount_dir.iterdir())[-1]
@@ -31,10 +42,11 @@ def test_do_backup_for_butterbackend(encrypted_btrfs_device) -> None:
                 file.relative_to(latest_folder / folder_dest_dir): file.read_bytes()
                 for file in list_files_recursively(latest_folder)
             }
+    latest_source_dir = source_directories[-1]
     expected_date = dt.date.today().isoformat()
     expected_content = {
-        file.relative_to(FIRST_BACKUP): file.read_bytes()
-        for file in list_files_recursively(FIRST_BACKUP)
+        file.relative_to(latest_source_dir): file.read_bytes()
+        for file in list_files_recursively(latest_source_dir)
     }
     assert expected_date in str(latest_folder)
     assert content == expected_content
