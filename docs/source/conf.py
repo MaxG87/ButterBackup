9eae1d5f6d557869c8ed70058d6eb50e1c33cf01
Max Görner <max@familie-goerner.eu>
2022-10-12 16:47:19 +0200

    Reformatiere conf.py
    Ansonsten meckert CI/CD.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,22 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'ButterBackup'
-copyright = '2022, Max Görner'
-author = 'Max Görner'
+project = "ButterBackup"
+copyright = "2022, Max Görner"
+author = "Max Görner"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary']
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosummary"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-language = 'de'
+language = "de"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
