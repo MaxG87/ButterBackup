58677a07e86dca9fe277913d3acec785d079869b
Max Görner <max@familie-goerner.eu>
2022-10-12 17:14:48 +0200

    Ersetze numpydoc mit napoleon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ author = "Max Görner"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosummary", "numpydoc"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosummary", "napoleon"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
