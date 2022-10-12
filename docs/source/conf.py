a7eba68d2d5b0ef8801d98bb7938275699cca962
Max Görner <max@familie-goerner.eu>
2022-10-12 16:55:01 +0200

    Füge numpydoc zu Sphinx-Konfiguration hinzu

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ author = "Max Görner"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosummary"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosummary", "numpydoc"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
