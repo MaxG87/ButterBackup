d0bf802da9a1011ccade0bdeee8c83058c7470bf
Max Görner <5477952+MaxG87@users.noreply.github.com>
2026-08-22 21:32:51 +0200

    test: Add back required fixture to per-project conftest

--- projects/butter-backup/tests/conftest.py
+++ projects/butter-backup/tests/conftest.py
@@ -1,8 +1,10 @@
 import itertools
 import typing as t
+from collections import abc
 from pathlib import Path
 
 import pytest
+import shell_interface as sh
 import storage_device_managers as sdm
 
 from butter_backup import backup_backends as bb
@@ -17,6 +19,24 @@ pytest_plugins = [
     "tests.fixtures.btrfs_rsync",
     "tests.fixtures.restic",
 ]
+
+
+@pytest.fixture
+def root_owned_tmp_path(tmp_path: Path) -> abc.Iterable[Path]:
+    """
+    Create a temporary directory owned by root for testing
+    """
+    root_owned_path = tmp_path / "root_owned"
+    root_owned_path.mkdir()
+    current_user = sh.get_user()
+    current_group = sh.get_group(current_user)
+    sh.chown(root_owned_path, user="root", group="root", recursive=False)
+    try:
+        yield root_owned_path
+    finally:
+        sh.chown(
+            root_owned_path, user=current_user, group=current_group, recursive=False
+        )
 
 
 @pytest.fixture(
