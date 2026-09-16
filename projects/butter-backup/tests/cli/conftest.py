8d1115b494cc1be2168b00afb77cb1aee7c405e7
Max Görner <5477952+MaxG87@users.noreply.github.com>
2026-09-16 22:09:13 +0200

    Switch from typer.echo to rich.print
    Typer's documentation explicitly recommends to use `rich.print`. That is
    much better anyways. So, all error messages in the CLI module are
    produced with rich.print now. Some other output related improvements
    were applied too.

--- projects/butter-backup/tests/cli/conftest.py
+++ projects/butter-backup/tests/cli/conftest.py
@@ -3,5 +3,9 @@ from typer.testing import CliRunner
 
 
 @pytest.fixture
-def runner():
+def runner(monkeypatch) -> CliRunner:
+    # Set the COLUMNS environment variable to a large value to avoid line wrapping in
+    # the output during tests. This ensures that the output is displayed in a single
+    # line, making it easier to assert error messages.
+    monkeypatch.setenv("COLUMNS", "1000")
     return CliRunner()
